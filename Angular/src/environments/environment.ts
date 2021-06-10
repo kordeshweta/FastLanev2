35875b6d90bcbb105e5b47661478bc834c2c52d0
RiyaKasat <riyakasat122@gmail.com>
2021-06-10 16:34:14 +0530

    PWA configured

--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -6,11 +6,10 @@ export const environment = {
   production: false,
   protfolioUrl: './assets/Data/protfolio.Json',
   subPortfolioUrl: './assets/Data/sub-portfolio.json',
-
   apiUrl: 'http://localhost:3000/api/',
   LocUrl: 'http://localhost:3000',
   clientId: '6a09f0e8-559b-4189-ab90-b8c75059fa2b',
   tanent: '02aa9fc1-18bc-4798-a020-e01c854dd434',
   redirectUri: 'http://localhost:4300/home',
-  postLogoutRedirectUri: 'http://localhost:4300/',
+  postLogoutRedirectUri: 'http://localhost:4300/'
 };
