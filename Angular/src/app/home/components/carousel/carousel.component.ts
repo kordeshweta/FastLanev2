fef412f7545f89b733df335d68c12eaa35aa8ce2
Anjali Sherikar <anjalisherikar@gmail.com>
2021-06-16 11:19:30 +0530

    disable left and right button

--- Angular/src/app/home/components/carousel/carousel.component.ts
+++ Angular/src/app/home/components/carousel/carousel.component.ts
@@ -28,27 +28,46 @@ export class CarouselComponent implements OnInit {
   }];
 
   startIndex;
+  disableLeft;
+  disableRight;
   constructor(){
-    this.startIndex = 0;
+    this.startIndex = 1;
   }
 
-  ngOnInit(){}
+  ngOnInit(){
+    this.disableLeft = true;
+  }
 
   slide(){
     const slides = Array.from(document.getElementsByClassName('content'));
-
+    console.log(slides.length)
+    console.log(this.startIndex)
     for (const x of slides) {
       const y = x as HTMLElement;
       y.style.display = 'none';
     }
 
-    if (this.startIndex > slides.length - 1) {
-      this.startIndex = 0;
-    } else if(this.startIndex < 0){
-      this.startIndex = slides.length - 1
+    if(this.startIndex > 1 && this.startIndex <= slides.length){
+      this.disableLeft = false;
+    }else{
+      this.disableLeft = true;
     }
 
-    const slide = slides[this.startIndex] as HTMLElement;
+    if(this.startIndex == slides.length){
+      this.disableRight = true;
+    }else{
+      this.disableRight = false;
+    }
+    // if (this.startIndex > slides.length - 1) {
+    //   //this.startIndex = 0;
+    //   this.disableRight = true;
+    // } else if(this.startIndex == 1){
+    //   //this.startIndex = slides.length - 1
+    //   this.disableLeft = true;
+
+    // }
+
+    const slide = slides[this.startIndex-1] as HTMLElement;
     slide.style.display = 'block';
   }
 
