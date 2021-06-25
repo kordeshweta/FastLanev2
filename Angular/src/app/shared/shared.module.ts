0b40c9d4f3a5513832cb27c560569d394c7bc474
RiyaKasat <riyakasat122@gmail.com>
2021-06-25 21:08:19 +0530

    Report Sec + Carousel added

--- Angular/src/app/shared/shared.module.ts
+++ Angular/src/app/shared/shared.module.ts
@@ -1,14 +1,14 @@
 //modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MaterialModule} from '../material.module';
+import { MaterialModule } from '../material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 //components and directives
-import {SearchComponentComponent} from './search-component/search-component.component';
-import {TileComponent} from './tile/tile.component';
-import {SelectAllCheckBoxComponent} from './select-all-check-box';
+import { SearchComponentComponent } from './search-component/search-component.component';
+import { TileComponent } from './tile/tile.component';
+import { SelectAllCheckBoxComponent } from './select-all-check-box';
 
 import { SelectCheckAllComponent } from '../shared/custom-material/select-check-all/select-check-all.component';
 import { DragDropDirective } from './drag-drop.directive';
@@ -25,12 +25,14 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 
 
+
 @NgModule({
   declarations: [
     SearchComponentComponent,
     TileComponent,
     SelectCheckAllComponent,
     MostPopularPipe,
+
     FilterArtifactPipe,
     DragDropDirective,
     SelectAllCheckBoxComponent,
@@ -48,4 +50,4 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
   exports: [SearchComponentComponent,TileComponent,SelectCheckAllComponent,MostPopularPipe,FilterArtifactPipe,SelectAllCheckBoxComponent,SlantSectionComponent,ClientFeedbackComponent],
 
 })
-export class SharedModule {}
+export class SharedModule { }
